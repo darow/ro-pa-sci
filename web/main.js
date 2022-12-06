38e1873116018b99d4b0ead855a2723835f50a9b
Daniyaki <daniil-yakimow@yandex.ru>
2022-12-06 21:19:21 +0300

    feat(web): refactor code

--- web/main.js
+++ web/main.js
@@ -6,69 +6,56 @@ const signupBtn = document.querySelector('#signup-btn')
 const logoutBtn = document.querySelector('#logout-btn')
 const usernameElem = document.querySelector('#username')
 const playersBtn = document.querySelector('#playersBtn')
-
-let ws = undefined
+let ws
 
 checkAuth(showPlayersTop)
 
-playersBtn.addEventListener("click", () => {
+playersBtn.addEventListener('click', () => {
     showPlayersTop()
 })
 
-loginBtn.addEventListener("click", () => {
-    fetch("/login.html")
-        .then((response) => response.text())
-        .then((data) => {
-            contentElem.innerHTML = data
-            document.forms['loginForm'].addEventListener('submit', (event) => {
-                event.preventDefault();
-                // TODO do something here to show user that form is being submitted
-                fetch(event.target.action, {
-                    method: 'POST',
-                    body: new URLSearchParams(new FormData(event.target)) // event.target is the form
-                }).then((resp) => {
-                    return resp.json();
-                }).then((body) => {
-                    if (body.error) {
-                        document.querySelector('#error').textContent = body.error
-                    } else {
-                        checkAuth(showPlayersTop)
-                    }
-                }).catch((error) => {
-                    console.log(error)
-                });
-            });
-        });
+loginBtn.addEventListener('click', () => {
+    try {
+        fetchTemplate('/login.html', 'loginForm')
+    } catch (error) {
+        console.log(error)
+    }
+})
+signupBtn.addEventListener('click', () => {
+    try {
+        fetchTemplate('/signup.html', 'signupForm')
+    } catch (error) {
+        console.log(error)
+    }
 })
 
-signupBtn.addEventListener("click", () => {
-    fetch("/signup.html")
-        .then((response) => response.text())
-        .then((data) => {
-            contentElem.innerHTML = data
-            document.forms['signupForm'].addEventListener('submit', (event) => {
-                event.preventDefault();
-                // TODO do something here to show user that form is being submitted
-                fetch(event.target.action, {
-                    method: 'POST',
-                    body: new URLSearchParams(new FormData(event.target)) // event.target is the form
-                }).then((resp) => {
-                    return resp.json();
-                }).then((body) => {
-                    if (body.error) {
-                        document.querySelector('#error').textContent = body.error
-                    } else {
-                        checkAuth(showPlayersTop)
-                    }
-                }).catch((error) => {
-                    console.log(error)
-                });
-            });
-        });
-})
+async function fetchTemplate(template, formName) {
+    const response = await fetch(template)
 
-logoutBtn.addEventListener("click", () => {
-    fetch("/auth/logout")
+    contentElem.innerHTML = await response.text()
+    document.forms[formName].addEventListener('submit', handleSubmitForm)
+}
+
+async function handleSubmitForm(event) {
+    event.preventDefault();
+
+    // TODO do something here to show user that form is being submitted
+    const response = await fetch(event.target.action, {
+        method: 'POST',
+        body: new URLSearchParams(new FormData(event.target)) // event.target is the form
+    })
+    const data = await response.json()
+
+    if (data.error) {
+        document.querySelector('#error').textContent = data.error
+        return
+    }
+
+    checkAuth(showPlayersTop)
+}
+
+logoutBtn.addEventListener('click', () => {
+    fetch('/auth/logout')
         .then((response) => {
                 if (response.status == 200) {
                     checkAuth()
@@ -80,73 +67,100 @@ logoutBtn.addEventListener("click", () => {
 })
 
 function refreshWS(callback) {
-    if (ws!==undefined&&ws.readyState === WebSocket.OPEN) {
-        ws.close()
+    closeWebSocket(ws)
+    ws = createWebSocket(`ws://${apiUri}/auth/ws`, { open: callback })
+}
+
+function createWebSocket(url, options, handlerCallbacks) {
+    if (!url) {
+        console.log('Не передан URL!')
     }
-    ws = new WebSocket(`ws://${apiUri}/auth/ws`);
-    configureWS(callback)
+
+    const eventNames = {
+        open: 'open',
+        close: 'close',
+        error: 'error',
+        message: 'message',
+    }
+
+    const callbacks = {
+        [eventNames.open]: () => {},
+        [eventNames.close]: () => {},
+        [eventNames.error]: () => {},
+        [eventNames.message]: () => {},
+        ...handlerCallbacks
+    }
+
+    const handlers = {
+         [eventNames.open]: () => {
+            console.log('Successfully Connected')
+            callbacks.open()
+        },
+        [eventNames.close]: event => {
+            console.log('Socket Closed Connection: ', event);
+            callbacks.close()
+        },
+        [eventNames.error]: error => {
+            console.log('Socket Error: ', error);
+            callbacks.error()
+        },
+        [eventNames.message]: () => {
+            console.log(`server message: ${e.data}`)
+            callbacks.message()
+        },
+    }
+
+    const webSocket = new WebSocket(url, options)
+
+    Object.values(eventNames).forEach(event => {
+        webSocket.addEventListener(event, handlers[event])
+    })
+
+    return webSocket
 }
 
-function configureWS(callback = () => {}) {
-    ws.onopen = () => {
-        console.log("Successfully Connected");
-        callback()
-    };
+function closeWebSocket(webSocket) {
+    if (!webSocket) {
+        return
+    }
 
-    ws.onclose = event => {
-        console.log("Socket Closed Connection: ", event);
-        ws.send("Client Closed!")
-    };
-
-    ws.onerror = error => {
-        console.log("Socket Error: ", error);
-    };
-
-    ws.onmessage = function (e) {
-        let server_message = e.data;
-        console.log(server_message);
-    };
+    webSocket.close()
 }
 
-function showPlayersTop() {
-    fetch("/online_users")
-        .then((response) => response.json())
-        .then((data) => {
-            let playersList = ""
-            let counter = 1
-            for (const [key, user] of Object.entries(data)) {
-                let btn = ``
-                if (user.is_online) {
-                    btn = `<btn id="invite-${user.id}" class="btn btn-sm btn-success btn-block">пригласить✉</btn>`
-                }
-                playersList += `<li class="m-1">${counter}. ${user.name} ${user.is_online} ${user.score} ${btn}</li>`
-                counter++
-            }
+async function showPlayersTop() {
+    const response = await fetch('/online_users')
+    const data = await response.json()
 
-            contentElem.innerHTML = playersTemplate.formatUnicorn({playerList: `${playersList}`});
+    const playersList = Object.values(data).map((user, index) => {
+        return `<li class="m-1">${index + 1}. ${getUserInfoElement(user)}</li>`
+    })
 
-        });
+    contentElem.innerHTML = playersTemplate.formatUnicorn({ playerList: playersList });
 }
 
-function checkAuth(callback) {
-    fetch("/auth/")
-        .then((response) => response.json())
-        .then((user) => {
-            logoutBtn.style.display = ""
-            loginBtn.style.display = "none"
-            signupBtn.style.display = "none"
-            usernameElem.innerHTML = user.name
-            refreshWS(callback)
-        }).catch((error) => {
-        logoutBtn.style.display = "none"
-        loginBtn.style.display = ""
-        signupBtn.style.display = ""
-        usernameElem.innerHTML = ""
+function getUserInfoElement({ name, is_online, score, id }) {
+    const inviteButton = `<btn id="invite-${id}" class="btn btn-sm btn-success btn-block">пригласить✉</btn>`
+
+    return `${name} ${is_online} ${score} ${inviteButton}`
+}
+
+async function checkAuth(callback) {
+    try {
+        const response = await fetch('/auth/')
+        const user = await response.json()
+
+        logoutBtn.hidden = false
+        loginBtn.hidden = true
+        signupBtn.hidden = true
+        usernameElem.innerHTML = user.name
+        refreshWS(callback)
+    } catch (error) {
+        logoutBtn.hidden = true
+        loginBtn.hidden = false
+        signupBtn.hidden = false
+        usernameElem.innerHTML = ''
         // if (webSocket!==undefined&&webSocket.readyState === WebSocket.OPEN) {
         //     webSocket.close()
         // }
-    });
+    }
 }
-
-
-
