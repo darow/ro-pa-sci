760fe44d54518f7f1b0d1af77d523ea6c57ed2d7
Daniyaki <daniil-yakimow@yandex.ru>
2023-01-07 18:25:13 +0300

    feat(frontend): added function getTopPlayers

---
+++ frontend/source/functions/getTopPlayers.js
@@ -0,0 +1,18 @@
+async function getTopPlayers(players) {
+    const playerList = Object.values(players)
+        .map((user, index) => {
+            const { name, is_online, score, id } = user;
+            const inviteButton = `<btn id="invite-${id}" class="btn btn-sm btn-success btn-block">пригласить✉</btn>`;
+
+            return `<li class="m-1">${index + 1}. ${name} ${is_online} ${score} ${inviteButton}</li>`;
+        });
+
+    return `<h3>Players Online</h3>
+        <div class="container">
+            <ul>
+                ${playerList}
+            </ul>
+        </div>`;
+}
+
+export default getTopPlayers;
---
+++ frontend/source/functions/index.js
@@ -0,0 +1,1 @@
+export { default as getTopPlayers } from './getTopPlayers';
